ad4cf4ed263c3d06013e3c2b2c2500918dd2cbd6
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-18 02:37:58 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,122 +1,15 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import Modal from "../Modal/Modal";
-import LoginPopup from "../Popups/LoginPopup/LoginPopup";
-import RegistrationPopup from "../Popups/RegistrationPopup/RegistrationPopup";
-import { useSelector } from "react-redux";
-import { selectUser } from "../../redux/authSlice";
-import { ReactComponent as IconAvatar } from "../../img/avatar.svg";
+import { Link } from "react-router-dom";
 import css from "./Header.module.css";
 
 const Header = () => {
-  const currentUser = useSelector(selectUser);
-
-  const [isOpenLoginPopup, setOpenLoginPopup] = useState(false);
-  const [isOpenRegistrationPopup, setOpenRegistrationPopup] = useState(false);
-
-  const handleOpenLoginPopup = () => {
-    setOpenLoginPopup(true);
-  };
-
-  const handleOpenRegistrationPopup = () => {
-    setOpenRegistrationPopup(true);
-  };
-
-  const closeModal = () => {
-    setOpenLoginPopup(false);
-    setOpenRegistrationPopup(false);
-  };
-
-  const handleLogOut = () => {};
-
   return (
     <header className={css.header}>
       <div className={css.container}>
         <nav className={css.navigation}>
-          {!currentUser ? (
-            <NavLink to="/" className={css.logo}>
-              Nanny.Services
-            </NavLink>
-          ) : (
-            <NavLink to="/nannies" className={css.logo}>
-              Nanny.Services
-            </NavLink>
-          )}
-
-          <div className={css.wraplistSpace}>
-            <div className={css.wraplist}>
-              <ul className={css.list}>
-                {!currentUser && (
-                  <li>
-                    <NavLink to="/" className={css.link}>
-                      Home
-                    </NavLink>
-                  </li>
-                )}
-                <li>
-                  <NavLink to="/nannies" className={css.link}>
-                    Nannies
-                  </NavLink>
-                </li>
-
-                {currentUser && (
-                  <li>
-                    <NavLink to="/favorites" className={css.link}>
-                      Favorites
-                    </NavLink>
-                  </li>
-                )}
-              </ul>
-              {currentUser ? (
-                <ul className={css.wrapButtonsLogout}>
-                  <li className={css.userWrap}>
-                    <IconAvatar />
-                    <p className={css.userName}>{currentUser.name}</p>
-                  </li>
-                  <li>
-                    <button
-                      type="button"
-                      className={css.btnLogout}
-                      onClick={handleLogOut}
-                    >
-                      Log out
-                    </button>
-                  </li>
-                </ul>
-              ) : (
-                <div className={css.wrapButtonsLogin}>
-                  <li>
-                    <button
-                      type="button"
-                      className={css.btnLogin}
-                      onClick={handleOpenLoginPopup}
-                    >
-                      Login
-                    </button>
-                  </li>
-                  {isOpenLoginPopup && (
-                    <Modal onClose={closeModal} title="Log In">
-                      <LoginPopup onClose={closeModal} />
-                    </Modal>
-                  )}
-                  <li>
-                    <button
-                      type="button"
-                      className={css.btnRegistration}
-                      onClick={handleOpenRegistrationPopup}
-                    >
-                      Registration
-                    </button>
-                  </li>
-                  {isOpenRegistrationPopup && (
-                    <Modal onClose={closeModal} title="Registration">
-                      <RegistrationPopup onClose={closeModal} />
-                    </Modal>
-                  )}
-                </div>
-              )}
-            </div>
-          </div>
+          <Link to="/" className={css.logo}>
+            <img src="../../img/Mask group.png" alt="logo" />
+            E-Pharmacy
+          </Link>
         </nav>
       </div>
     </header>
