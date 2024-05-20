11477fee7a9ce6b73f5dd23db12c868970a5d507
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-20 16:47:42 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,52 @@
 import { Link } from "react-router-dom";
 import css from "./Header.module.css";
+import LogoAuthImage from "../../img/logoAuth.png";
+import Icon from "../Icon";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logOutThunk } from "../../redux/auth/operations";
+// import { useSelector } from "react-redux";
+// import { selectCurrentUser } from "../../redux/auth/selectors";
 
 const Header = () => {
+  // const currentUser = useSelector(selectCurrentUser);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [iconName, setIconName] = useState("logout");
+
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logOutThunk() as any);
+  };
+
   return (
     <header className={css.header}>
-      <div className={css.container}>
-        <nav className={css.navigation}>
-          <Link to="/" className={css.logo}>
-            <img src="../../img/Mask group.png" alt="logo" />
-            E-Pharmacy
-          </Link>
-        </nav>
+      <nav className={css.navigation}>
+        {openMenu ? (
+          <div onClick={() => setOpenMenu(false)} className={css.icon}>
+            <Icon name="close" width={32} height={32} />
+          </div>
+        ) : (
+          <div onClick={() => setOpenMenu(true)} className={css.icon}>
+            <Icon name="menu-burger" width={32} height={32} />
+          </div>
+        )}
+        <Link to="/" className={css.logo}>
+          <img src={LogoAuthImage} alt="logo" />
+          <div>
+            <p className={css.logoTitle}>Medicine store</p>
+            <p className={css.logotext}>Dasboard | vendor@gmail.com</p>
+            {/* <p>Dasboard | {currentUser.email}</p> */}
+          </div>
+        </Link>
+      </nav>
+      <div
+        className={css.iconLogout}
+        onMouseEnter={() => setIconName("logout-active")}
+        onMouseLeave={() => setIconName("logout")}
+        onClick={handleLogOut}
+      >
+        <Icon name={iconName} width={44} height={44} />
       </div>
     </header>
   );
