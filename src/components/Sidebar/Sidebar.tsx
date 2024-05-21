b6e3f9812561f0ed2a834d086f9cd772efee8ab7
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-21 11:05:23 +0300

    fix sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,56 +1,65 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import Icon from "../Icon";
 import css from "./Sidebar.module.css";
 import { NavLink } from "react-router-dom";
 
 interface IProps {
   onClose: () => void;
+  isOpen: boolean;
 }
 
-const Sidebar: FC<IProps> = ({ onClose }) => {
-  const [openMenu, setOpenMenu] = useState(false);
-
+const Sidebar: FC<IProps> = ({ onClose, isOpen }) => {
   const handleLogOut = () => {};
 
   return (
-    <div className={css.sidebar}>
-      <nav className={css.navigation}>
-        <div onClick={onClose} className={css.icon}>
-          <Icon name="close" width={32} height={32} />
+    <div
+      className={`${css.backdrop} ${
+        isOpen ? css.showBackdrop : css.hideBackdrop
+      }`}
+    >
+      <div className={css.sidebar}>
+        <nav className={css.navigation}>
+          <div onClick={onClose} className={css.icon}>
+            <Icon name="close" width={32} height={32} />
+          </div>
+
+          <ul className={css.list}>
+            <li className={css.wraplink}>
+              <NavLink to="/dashboard" className={css.link}>
+                <Icon name="dashboard" width={12} height={12} />
+              </NavLink>
+            </li>
+
+            <li className={css.wraplink}>
+              <NavLink to="/orders" className={css.link}>
+                <Icon name="busket" width={13} height={13} />
+              </NavLink>
+            </li>
+
+            <li className={css.wraplink}>
+              <NavLink to="/products" className={css.link}>
+                <Icon name="bottle" width={12} height={13} />
+              </NavLink>
+            </li>
+
+            <li className={css.wraplink}>
+              <NavLink to="/suppliers" className={css.link}>
+                <Icon name="bag" width={12} height={13} />
+              </NavLink>
+            </li>
+
+            <li className={css.wraplink}>
+              <NavLink to="/customers" className={css.link}>
+                <Icon name="users" width={13} height={10} />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={css.iconLogout} onClick={handleLogOut}>
+          <Icon name="phone" width={13} height={14} />
         </div>
-
-        <ul className={css.list}>
-          <li>
-            <NavLink to="/dashboard" className={css.link}>
-              <Icon name="dashboard" />
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/orders" className={css.link}>
-              <Icon name="orders" />
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/products" className={css.link}>
-              <Icon name="products" />
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/suppliers" className={css.link}>
-              <Icon name="suppliers" />
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink to="/customers" className={css.link}>
-              <Icon name="customers" />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      </div>
     </div>
   );
 };
