90a816c08302abe155da7442cc9e2b769d1006ee
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-22 14:07:46 +0300

    add button

---
+++ src/components/AllProducts/AddNewProduct/AddNewProduct.tsx
@@ -0,0 +1,12 @@
+import css from "./AddNewProduct.module.css";
+import Icon from "../../Icon";
+
+const AddNewProduct = () => {
+  return (
+    <div className={css.icon}>
+      <Icon name="add" width={20} height={20} />
+    </div>
+  );
+};
+
+export default AddNewProduct;
