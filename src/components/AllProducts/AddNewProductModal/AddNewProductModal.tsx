4eff98422f6324b91d64a2c6140bfd6bc4a1eb61
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-23 12:36:46 +0300

    fix add modal

--- src/components/AllProducts/AddNewProductModal/AddNewProductModal.tsx
+++ src/components/AllProducts/AddNewProductModal/AddNewProductModal.tsx
@@ -1,7 +1,13 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import css from "./AddNewProductModal.module.css";
 import Icon from "../../Icon";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { categories } from "../../../data/categories";
+import { nanoid } from "nanoid";
+import useCloseDropdown from "../../../services/closeDropdown";
+import Dropdown from "../../Dropdown/Dropdown";
 
 interface AddModalProps {
   onClose: () => void;
@@ -16,10 +22,45 @@ interface IForms {
 }
 
 const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
-  const { register, handleSubmit } = useForm<IForms>();
+  const [isOpenDropdown, setOpenDropdown] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  const dropdownref = useRef<HTMLDivElement | null>(null);
+  const iconref = useRef<HTMLDivElement | null>(null);
+
+  const schema = yup
+    .object({
+      productInfo: yup.string().required("Product info is required"),
+      category: yup.string().required("Category is required"),
+      suppliers: yup.string().required("Suppliers is required"),
+      stock: yup.string().required("Stock is required"),
+      price: yup
+        .number()
+        .typeError("Price is required and must be a number")
+        .required(),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IForms>({
+    resolver: yupResolver(schema),
+  });
+
   const onSubmit = (data: IForms) => {
     console.log(data);
+    reset();
   };
+
+  const handleSelect = (selectedFilter: string) => {
+    setSelectedFilter(selectedFilter);
+    // dispatch(setFilter(selectedFilter));
+  };
+
+  useCloseDropdown(onClose, dropdownref, iconref);
 
   return (
     <>
@@ -30,17 +71,32 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
             className={css.input}
             placeholder="Product info"
           />
+          <p className={css.errormessage}>{errors.productInfo?.message}</p>
 
           <div className={css.inputWrap}>
             <input
               {...register("category")}
               className={css.input}
               placeholder="Category"
+              value={selectedFilter}
             />
+            <p className={css.errormessage}>{errors.category?.message}</p>
 
-            <div className={css.iconChevron}>
+            <div
+              className={css.iconChevron}
+              onClick={() => setOpenDropdown(!isOpenDropdown)}
+              ref={iconref}
+            >
               <Icon name="chevron-down" width={16} height={16} />
             </div>
+
+            {isOpenDropdown && (
+              <Dropdown
+                onSelect={handleSelect}
+                onClose={onClose}
+                ref={iconref}
+              />
+            )}
           </div>
 
           <input
@@ -48,18 +104,22 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
             className={css.input}
             placeholder="Suppliers"
           />
+          <p className={css.errormessage}>{errors.suppliers?.message}</p>
 
           <input
             {...register("stock")}
             className={css.input}
             placeholder="Stock"
           />
+          <p className={css.errormessage}>{errors.stock?.message}</p>
 
           <input
             {...register("price")}
             className={css.input}
             placeholder="Price"
+            type="number"
           />
+          <p className={css.errormessage}>{errors.price?.message}</p>
         </div>
 
         <div className={css.buttonWrap}>
@@ -67,7 +127,7 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
             Add
           </button>
 
-          <button type="submit" className={css.buttonCancel}>
+          <button type="button" className={css.buttonCancel} onClick={onClose}>
             Cancel
           </button>
         </div>
