5fcb66a1af9e19547a7b6c5e837543b31395e22f
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-23 19:00:26 +0300

    fix

--- src/components/AllProducts/AddNewProductModal/AddNewProductModal.tsx
+++ src/components/AllProducts/AddNewProductModal/AddNewProductModal.tsx
@@ -4,9 +4,6 @@ import Icon from "../../Icon";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { categories } from "../../../data/categories";
-import { nanoid } from "nanoid";
-import useCloseDropdown from "../../../services/closeDropdown";
 import Dropdown from "../../Dropdown/Dropdown";
 
 interface AddModalProps {
@@ -25,7 +22,6 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
   const [isOpenDropdown, setOpenDropdown] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState("");
 
-  const dropdownref = useRef<HTMLDivElement | null>(null);
   const iconref = useRef<HTMLDivElement | null>(null);
 
   const schema = yup
@@ -55,23 +51,23 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
     reset();
   };
 
-  const handleSelect = (selectedFilter: string) => {
-    setSelectedFilter(selectedFilter);
-    // dispatch(setFilter(selectedFilter));
+  const handleSelect = (selected: string) => {
+    setSelectedFilter(selected);
+    setOpenDropdown(false);
   };
-
-  useCloseDropdown(onClose, dropdownref, iconref);
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={css.wrap}>
-          <input
-            {...register("productInfo")}
-            className={css.input}
-            placeholder="Product info"
-          />
-          <p className={css.errormessage}>{errors.productInfo?.message}</p>
+          <div>
+            <input
+              {...register("productInfo")}
+              className={css.input}
+              placeholder="Product info"
+            />
+            <p className={css.errormessage}>{errors.productInfo?.message}</p>
+          </div>
 
           <div className={css.inputWrap}>
             <input
@@ -89,37 +85,42 @@ const AddNewProductModal: FC<AddModalProps> = ({ onClose }) => {
             >
               <Icon name="chevron-down" width={16} height={16} />
             </div>
-
             {isOpenDropdown && (
               <Dropdown
                 onSelect={handleSelect}
-                onClose={onClose}
+                onClose={setOpenDropdown}
                 ref={iconref}
               />
             )}
           </div>
 
-          <input
-            {...register("suppliers")}
-            className={css.input}
-            placeholder="Suppliers"
-          />
-          <p className={css.errormessage}>{errors.suppliers?.message}</p>
+          <div>
+            <input
+              {...register("suppliers")}
+              className={css.input}
+              placeholder="Suppliers"
+            />
+            <p className={css.errormessage}>{errors.suppliers?.message}</p>
+          </div>
 
-          <input
-            {...register("stock")}
-            className={css.input}
-            placeholder="Stock"
-          />
-          <p className={css.errormessage}>{errors.stock?.message}</p>
+          <div>
+            <input
+              {...register("stock")}
+              className={css.input}
+              placeholder="Stock"
+            />
+            <p className={css.errormessage}>{errors.stock?.message}</p>
+          </div>
 
-          <input
-            {...register("price")}
-            className={css.input}
-            placeholder="Price"
-            type="number"
-          />
-          <p className={css.errormessage}>{errors.price?.message}</p>
+          <div>
+            <input
+              {...register("price")}
+              className={css.input}
+              placeholder="Price"
+              type="number"
+            />
+            <p className={css.errormessage}>{errors.price?.message}</p>
+          </div>
         </div>
 
         <div className={css.buttonWrap}>
