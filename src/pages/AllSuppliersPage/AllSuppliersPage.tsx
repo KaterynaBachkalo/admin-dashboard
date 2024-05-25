c93bbd4311fceda2d77d2570cda5922979e4cb16
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-25 15:34:55 +0300

    add components

--- src/pages/AllSuppliersPage/AllSuppliersPage.tsx
+++ src/pages/AllSuppliersPage/AllSuppliersPage.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./AllSuppliersPage.module.css";
+import FilterForm from "../../components/FilterForm/FilterForm";
+import AddNewSupplier from "../../components/AllSuppliers/AddNewSupplier/AddNewSupplier";
+import AllSuppliersTable from "../../components/AllSuppliers/AllSuppliersTable/AllSuppliersTable";
 
-const AllOrdersPage = () => {
-  return <section className={css.container}>AllOrdersPage</section>;
+const AllSuppliersPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <section className={css.container}>
+      <FilterForm setSearchQuery={setSearchQuery} placeholder="User Name" />
+      <AddNewSupplier />
+      <AllSuppliersTable searchQuery={searchQuery} />
+    </section>
+  );
 };
 
-export default AllOrdersPage;
+export default AllSuppliersPage;
