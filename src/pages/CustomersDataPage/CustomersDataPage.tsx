4e97f7770d1629f1040626e352b865eb68ada6f6
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-25 15:34:35 +0300

    fix

--- src/pages/CustomersDataPage/CustomersDataPage.tsx
+++ src/pages/CustomersDataPage/CustomersDataPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import css from "./CustomersDataPage.module.css";
 
 const CustomersDataPage = () => {
-  return <section className={css.container}>AllOrdersPage</section>;
+  return <section className={css.container}>CustomersDataPage</section>;
 };
 
 export default CustomersDataPage;
