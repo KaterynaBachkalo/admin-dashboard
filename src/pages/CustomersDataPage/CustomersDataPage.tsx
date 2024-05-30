3e816e576f78e8b3277ea66465fb70f5a4a8a436
KaterynaBachkalo <kozlovets88@gmail.com>
2024-05-30 14:18:42 +0300

    fix page

--- src/pages/CustomersDataPage/CustomersDataPage.tsx
+++ src/pages/CustomersDataPage/CustomersDataPage.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./CustomersDataPage.module.css";
+import FilterForm from "../../components/FilterForm/FilterForm";
+import AllCustomersTable from "../../components/AllCustomers/AllCustomersTable";
 
 const CustomersDataPage = () => {
-  return <section className={css.container}>CustomersDataPage</section>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <section className={css.container}>
+      <FilterForm setSearchQuery={setSearchQuery} placeholder="User name" />
+      <AllCustomersTable searchQuery={searchQuery} />
+    </section>
+  );
 };
 
 export default CustomersDataPage;
