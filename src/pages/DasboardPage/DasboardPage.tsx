26a0b28632d254578cbf93ab66cbc9bd6bee8b6f
KaterynaBachkalo <kozlovets88@gmail.com>
2024-06-14 21:07:01 +0200

    fix

--- src/pages/DasboardPage/DasboardPage.tsx
+++ src/pages/DasboardPage/DasboardPage.tsx
@@ -2,8 +2,18 @@ import css from "./DasboardPage.module.css";
 import Statistics from "../../components/Dashboard/Statistics/Statistics";
 import RecentCustomers from "../../components/Dashboard/RecentCustomers/RecentCustomers";
 import IncomeExpenses from "../../components/Dashboard/IncomeExpenses/IncomeExpenses";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { fetchData } from "../../redux/admin/operation";
 
 const DasboardPage = () => {
+  const dispatch = useDispatch() as AppDispatch;
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
     <section className={css.container}>
       <Statistics />
