2548852c4b1319ec9040e79c3f13c021923023e1
KaterynaBachkalo <kozlovets88@gmail.com>
2024-06-15 22:26:21 +0200

    fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { SharedLayout } from "./components/SharedLayout/SharedLayout";
-import { lazy, useEffect } from "react";
+import { lazy } from "react";
 
 import RestrictedRoute from "./components/RestrictedRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import { useDispatch } from "react-redux";
-import { refreshUserThunk } from "./redux/auth/operations";
-import { AppDispatch } from "./redux/store";
+import { useSelector } from "react-redux";
+import { selectAuthIsLoading } from "./redux/auth/selectors";
+import Loader from "./components/Loader/Loader";
 
-const AllProductsPage = lazy(
-  () => import("./pages/AllProductsPage/AllProductsPage")
-);
-const DasboardPage = lazy(() => import("./pages/DasboardPage/DasboardPage"));
-const AllOrdersPage = lazy(() => import("./pages/AllOrdersPage/AllOrdersPage"));
-const AllSuppliersPage = lazy(
-  () => import("./pages/AllSuppliersPage/AllSuppliersPage")
-);
-const CustomersDataPage = lazy(
-  () => import("./pages/CustomersDataPage/CustomersDataPage")
-);
+const AllProductsPage = lazy(() => import("./pages/AllProductsPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage/DashboardPage"));
+const AllOrdersPage = lazy(() => import("./pages/AllOrdersPage"));
+const AllSuppliersPage = lazy(() => import("./pages/AllSuppliersPage"));
+const CustomersDataPage = lazy(() => import("./pages/CustomersDataPage"));
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
+  const isLoading = useSelector(selectAuthIsLoading);
 
-  useEffect(() => {
-    dispatch(refreshUserThunk());
-  }, [dispatch]);
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <>
@@ -41,12 +35,13 @@ function App() {
             <PrivateRoute component={<SharedLayout />} redirectTo="/login" />
           }
         >
-          <Route path="/" element={<Navigate to="dashboard" />} />
-          <Route path="dashboard" element={<DasboardPage />} />
+          <Route path="/" index element={<Navigate to="dashboard" />} />
+          <Route path="dashboard" element={<DashboardPage />} />
           <Route path="orders" element={<AllOrdersPage />} />
           <Route path="products" element={<AllProductsPage />} />
           <Route path="suppliers" element={<AllSuppliersPage />} />
           <Route path="customers" element={<CustomersDataPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </>
